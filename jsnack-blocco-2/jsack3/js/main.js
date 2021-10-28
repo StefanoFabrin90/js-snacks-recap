7e5bd4760798a6479cd1ce7808c877f916263665
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-28 16:24:18 +0200

    blocco 2 js3

--- jsnack-blocco-2/jsack3/js/main.js
+++ jsnack-blocco-2/jsack3/js/main.js
@@ -11,12 +11,3 @@ let cognomiRandom = Math.floor(Math.random()*cognomi.length);
 console.log(cognomi[cognomiRandom]);
 
 document.getElementById('lista').innerHTML = `${nomi[nomiRandom]} ${cognomi[cognomiRandom]}`;
-
-// var stringhe = [
-//     "1- Lorem ipsum dolor sit amet, consectetur.",
-//     "2- Nulla imperdiet, est non laoreet laoreet.",
-//     "3- Ut sagittis eu lorem eget semper",
-//  ];
-//  var randomNum = Math.floor(Math.random()*stringhe.length);
- 
-//  document.write(stringhe[randomNum]);
